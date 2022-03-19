6c9bf45526146ab053bd9b93706cad08d68781c1
Robert Wilson <51052058+scorfano@users.noreply.github.com>
2022-03-19 14:00:51 +0100

    update

---
+++ src/BlazorWerks/Bootstrap/BsModalOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWerks.Bootstrap
+{
+    public class BsModalOptions
+    {
+        /// <summary>
+        /// boolean or the string 'static'	true  Includes a modal-backdrop element. Alternatively, specify static for a backdrop which doesn't close the modal on click.
+        /// </summary>
+        public bool? Backdrop { get; set; }
+
+        /// <summary>
+        /// Closes the modal when escape key is pressed
+        /// </summary>
+        public bool? Keyboard { get; set; }
+
+        /// <summary>
+        /// Puts the focus on the modal when initialized.
+        /// </summary>
+        public bool? Focus { get; set; }
+
+    }
+}
